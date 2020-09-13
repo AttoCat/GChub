49d7d1c214c6fa16fb823e36f047375cdad919c8
Shirataki2 <ahahah114514@gmail.com>
2020-09-13 11:47:57 +0900

    [ADD] base scripts to launch

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import discord
+from discord.ext import commands
+
+
+class GCBot(commands.Bot):
+    def __init__(self, prefix='$', **kwargs):
+        super().__init__(command_prefix=prefix, **kwargs)
+        self._load_cogs()
+
+    def _load_cogs(self):
+        cogs = ['cogs.global_chat']
+        for cog in cogs:
+            self.load_extension(cog)
+
+if __name__ == "__main__":
+    bot = GCBot()
+    bot.run(os.environ['TOKEN'])
