60d0b360163e77830b44693a04794dfedfeb3583
daima3629 <daima3629@usbc.be>
2020-09-28 19:04:52 +0900

    [Add] get_gchat_channelsメソッドを追加

--- lib/database.py
+++ lib/database.py
@@ -1,6 +1,22 @@
 import asyncpg
-from typing import Union, Any  # Tupleがあったけど使われてないので削除。
+from typing import Union, Any, List
+from dataclasses import dataclass
 import os
+
+
+@dataclass
+class Gchat:
+    """Class for global chat data."""
+    gchat_id: str
+    owner_id: int
+    password: str
+
+
+@dataclass
+class GchatChannel:
+    """Class for global chat channel data."""
+    channel_id: int
+    gchat_id: str
 
 
 class Database:
@@ -8,34 +24,30 @@ class Database:
         self.bot = bot
         self.conn: Union[asyncpg.Connection, None] = None
 
-    async def check_database(self) -> None:
-        conn = self.conn or await self.setup()
+    async def check_database(self, conn: asyncpg.Connection) -> None:
         try:
-            await conn.execute('SELECT "gchat"::regclass')
+            await self.conn.execute('SELECT "gchat"::regclass')
         except asyncpg.exceptions.UndefinedColumnError:
-            await conn.execute('''
+            await self.conn.execute('''
                 CREATE TABLE gchat (
-                    gchat_id varchar(20)
-                    owner_id bigint
+                    gchat_id varchar(20) PRIMARY KEY,
+                    owner_id bigint,
                     password varchar(100)
-
-                    PRIMARY KEY(gchat_id)
                 )
             ''')
         try:
-            await conn.execute('SELECT "gchat_channels"::regclass')
+            await self.conn.execute('SELECT "gchat_channels"::regclass')
         except asyncpg.exceptions.UndefinedColumnError:
-            await conn.execute('''
+            await self.conn.execute('''
                 CREATE TABLE gchat_channels (
-                    channel_id bigint
-                    gchat_id varchar(20)
+                    channel_id bigint PRIMARY KEY,
+                    gchat_id varchar(20),
 
-                    PRIMARY KEY(channel_id)
                     FOREIGN KEY(gchat_id) REFERENCES global_chat(gchat_id)
                 )
             ''')
 
-    async def create_connection(self) -> asyncpg.Connection:
+    async def setup_connection(self) -> asyncpg.Connection:
         self.conn = await asyncpg.connect(
             host='localhost',
             port=12358,
@@ -44,8 +56,21 @@ class Database:
             database=os.environ["POSTGRES_DB"],
             loop=self.bot.loop
         )
+        await self.check_database()
         return self.conn
 
     async def close(self) -> None:
         if self.conn is not None:
             await self.conn.close()
+
+    async def get_gchat_channels(self, gchat_id: str) -> List[GchatChannel]:
+        conn = self.conn or self.setup_connection()
+        channel_records = await conn.fetch(f'SELECT * FROM ghat_chnnels WHERE gchat_id="{gchat_id}"')
+        channel_object_list: List[GchatChannel] = []
+        for channel_tuple in channel_records:
+            gchat_channel = GchatChannel(
+                channel_id=channel_tuple[0],
+                gchat_id=channel_tuple[1]
+            )
+            channel_object_list.append(gchat_channel)
+        return channel_object_list
