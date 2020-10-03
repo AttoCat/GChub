a0a7fbc639e8d4d5e8754d09f4445149597f62e5
mov-huyu <mov.huyu@gmail.com>
2020-10-03 23:02:51 +0900

    [FIX]コメントアウトの空白追加

--- cogs/join.py
+++ cogs/join.py
@@ -8,10 +8,10 @@ class Join(commands.Cog):
 
     @commands.command()
     async def join(self, ctx):
-    #変数 
-        open_channel_name = ['test'] #すべてのパスのないチャンネル名
-        close_channel_name = ['TEST']#すべてのパスのあるチャンネル名
-    #取得
+    # 変数 
+        open_channel_name = ['test'] # すべてのパスのないチャンネル名
+        close_channel_name = ['TEST']# すべてのパスのあるチャンネル名
+    # 取得
         dm = await ctx.author.create_dm()
         await dm.send("※テキストチャンネルで設定すると情報漏洩の原因となりますのでDMで設定します。\nご了承ください。\n**注意： 全ての入力は1分経過するとタイムアウトとなり、やり直しになります。\n事前に入力内容を決めておき、メモ帳に書いておく等することをお勧めします。**")
         await dm.send("グローバルチャットチャンネルに参加します。\n参加したいグローバルチャンネル名を記入してください。")
@@ -22,20 +22,20 @@ class Join(commands.Cog):
         except asyncio.TimeoutError:
             await dm.send('タイムアウトしました\nもう一度最初からお試しください')
             return
-    #例外
+    # 例外
         if chname.content not in (*open_channel_name , *close_channel_name):
             await dm.send('存在しないチャンネルです。もう一度ご確認ください')
             return
-    #既存
+    # 既存
         else:
             new_channel_name = chname.content
-        #open
+        # open
             if chname.content in open_channel_name:
-            #DB書き込み
+            # DB書き込み
                 await ctx.send('認証しました。\n作成中・・・')
-        #close
+        # close
             elif chname.content in close_channel_name:
-                #DBからchname.contentでpass取得
+                # DBからchname.contentでpass取得
                 password='pass'
                 await dm.send('パスワード付きのチャンネルです\nパスワードを入力してください。')
                 def check2(m):
@@ -55,17 +55,17 @@ class Join(commands.Cog):
                         await dm.send('タイムアウトしました\nもう一度最初からお試しください')
                         return
                     if pass_word.content == password:
-                    #DB書き込み
+                    # DB書き込み
                         await ctx.send('認証しました。\n作成中・・・')
                     else:
                         await dm.send('パスワードが違います\nもう一度最初から設定してください')
                         return
                 else:
-                #DB書き込み
+                # DB書き込み
                     await ctx.send('認証しました。\n作成中・・・')
 
-        #DB書き込みはここに
-        #作成
+        # DB書き込みはここに
+        # 作成
             await ctx.send(new_channel_name)
             new_chan = await ctx.guild.create_text_channel(name=new_channel_name) 
             await new_chan.create_webhook(name="GChub")
